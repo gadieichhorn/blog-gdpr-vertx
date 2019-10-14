5355d2e38812064dd9607f3df5fa0d1cd773dd57
gadi eichhorn <gadi.eichhorn@gmail.com>
2019-10-14 12:46:46 +0100

    add crypto AES

--- src/main/java/com/rds/gdpr/patterns/model/ChatMessage.java
+++ src/main/java/com/rds/gdpr/patterns/model/ChatMessage.java
@@ -15,6 +15,14 @@ public class ChatMessage {
     @NonNull
     private String message;
 
-    private String publickKey;
+    private String key;
+
+//    @Builder
+//    public ChatMessage(@NonNull String from, @NonNull String message) {
+//        this.from = from;
+//        this.message = message;
+//        final SecretKeySpec secretKey = new SecretKeySpec(UUID.randomUUID().toString().substring(0, 16).getBytes(), "AES");
+//        this.key = Binary.builder().data(secretKey.getEncoded()).build();
+//    }
 
 }
